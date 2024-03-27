011d715587b94aa696304345cb66cbcd5904b4b1
Süleyman Ezdemir <ezfesoftware@gmail.com>
2024-03-27 12:31:27 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mobil_w5/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobil_w5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mobil Hafta 5',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Mobil Hafta 5"),
+      ),
+      body: Center(
+        child: 
+          
+          //--------------
+          //--------------
+          //--------------Kodları aşağıya yazınız.
+          //--------------
+          //--------------
+          //--------------    
+          //--------------
+          //--------------
+          //--------------Kodları yukarıya yazınız.
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          //--------------
+          ),
+    );
+  }
+}
